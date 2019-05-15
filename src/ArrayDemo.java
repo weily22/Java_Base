e71b256ef5540b592c094455f43dec1cb5df8a6e
weily <weily74596@163.com>
2019-05-15 16:06:02 +0800

    添加数组

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -26,12 +26,19 @@ public class ArrayDemo {
   public static void arraysTest() {
     int[] a = {3, 4, 5, 6};
     int[] a2 = {3, 4, 5, 6};
-    boolean bool = (a == a2);
+    // boolean bool = (a == a2);
     System.out.println("a数组和a2数组是否相等：" + Arrays.equals(a, a2));
-    System.out.println("a数组和a2数组是否相等==：" + bool);
+    System.out.println("a数组和a2数组是否相等==：" + (a == a2));
     // 通过复制a数组，生成一个新的b数组
-    int[] b = Arrays.copyOf(a, 4);
+    int[] b = Arrays.copyOf(a, 6);
     System.out.println("a数组和b数组是否相等：" + Arrays.equals(a, b));
+    System.out.println("b数组的元素为：" + Arrays.toString(b));
+    // 将b数组的第三个元素到第5个元素即区间[3,5)赋值为1
+    Arrays.fill(b, 2, 4, 1);
+    System.out.println("b数组此时的元素为：" + Arrays.toString(b));
+    // 对b数组进行排序
+    Arrays.sort(b);
+    System.out.println("排序后的b数组的元素为：" + Arrays.toString(b));
   }
  
 }
